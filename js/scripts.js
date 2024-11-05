0589b4e17a5f29398ba2ab1ad360f16974d0ff9e
Dennys Moreira <nickyparra0@gmail.com>
2024-11-05 16:41:12 -0500

    Correciones

--- js/scripts.js
+++ js/scripts.js
@@ -175,11 +175,10 @@ $(document).ready(function () {
             type: 'fraction',
         },
 
-
         renderFraction: function (currentClass, totalClass) {
-            return `<span class="${0 + currentClass}" ></span>' +
-                ' / ' +
-                '<span class="${0 + totalClass}" ></span>`;
+            return `<span class="${0 + currentClass} "></span>' +
+                  ' / ' +
+                  '<span class="${0 + totalClass} "></span>`;
         }
     });
 
